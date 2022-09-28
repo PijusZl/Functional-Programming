13ab88c1554a20cae754c8df4d7ce458df189110
majne <majne@LAPTOP-M7H959U6>
2022-09-29 01:38:32 +0300

    gameStart and show

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -1,36 +1,68 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Lib1(
     State, emptyState, gameStart, render, mkCheck, toggle, hint
 ) where
 
 import Types
+import Data.Char(digitToInt)
 
--- This is a state of your game.
--- It must contain all values you might need during a game:
--- number of occupied rows/cols, hints, occupied cells,..
--- You can change the right hand side as you wish but please
--- keep the type name as is
 data State = State {
-                    rows :: [Int],
                     cols :: [Int],
+                    rows :: [Int],
                     ships :: [((Int, Int), Bool)]
                    }
                     deriving Show
 
--- IMPLEMENT
--- This is very initial state of your program
 emptyState :: State
-emptyState = State ["Initial state"]
+emptyState = State {cols = [], rows = [], ships = []} --pradedam su tusciais masyvais
 
--- IMPLEMENT
--- This adds game data to initial state 
 gameStart :: State -> Document -> State
-gameStart (State l) d = State $ ("Game started: " ++ show d) : l
+gameStart (State c r s) d = gameStart'(State c r s) (show d) --document pakeiciam jo stringu
+
+gameStart' :: State -> String -> State 
+gameStart' (State c r s) [] = State{cols = c, rows = r, ships = s}
+gameStart' (State c r s) (x:xs) = 
+    if x == 'M' -- skaito kol randa M (for DMap)
+        then writeCols (State c r s) 0 xs
+            else gameStart' (State c r s) xs
+
+writeCols :: State -> Int -> String -> State 
+writeCols (State c r s) n [] = State{cols = c, rows = r, ships = s}
+writeCols (State c r s) n (x:xs) = 
+    if n == 10 --kadangi lentele 10x10 skaito kol perskaito 10 cols
+        then writeRows (State c r s) 0 xs
+            else 
+                if (x == '0' || x == '1' || x == '2' || x == '3' || x == '4' || x == '5' || x == '6' || x == '7' || x == '8' || x == '9')
+                    then writeCols (addCols (State c r s) (digitToInt x)) (n + 1) xs --digitToInt :: char -> int
+                        else writeCols (State c r s) n xs
+
+addCols :: State -> Int -> State
+addCols (State c r s) x = State {cols = x : c, rows = r, ships = s }
+
+writeRows :: State -> Int -> String -> State
+writeRows (State c r s) n [] = State{cols = c, rows = r, ships = s}
+writeRows (State c r s) n (x:xs) = 
+    if n == 10
+        then State {cols = c, rows = r, ships = s} -- grizti po sito, nes baigesi rows
+            else 
+                if x == '0' || x == '1' || x == '2' || x == '3' || x == '4' || x == '5' || x == '6' || x == '7' || x == '8' || x == '9'
+                    then writeRows (addRows (State c r s) (digitToInt x)) (n + 1) xs
+                        else writeRows (State c r s) n xs
+
+addRows :: State -> Int -> State
+addRows (State c r s) x = State {cols = c, rows = x : r, ships = s}
 
 -- IMPLEMENT
 -- renders your game board
 render :: State -> String
-render = show
+render (State c r s) = "    0  1  2  3  4  5  6  7  8  9\n    " ++ showCols c ++ showRows r 0
+
+showCols :: [Int] -> String
+showCols [] = "\n"
+showCols (x:xs) = show x ++ "  " ++ showCols xs
+
+showRows :: [Int] -> Int -> String
+showRows [] _ = " "                           --0  1  2  3  4  5  6  7  8  9
+showRows (x:xs) a = show a ++ " " ++ show x ++ " __|__|__|__|__|__|__|__|__|__\n" ++ showRows xs (a + 1)
 
 -- IMPLEMENT
 -- Make check from current state
@@ -41,11 +73,11 @@ mkCheck _ = Check []
 -- Toggle state's value
 -- Receive raw user input tokens
 toggle :: State -> [String] -> State
-toggle (State l) t = State $ ("Toggle " ++ show t) : l
+toggle (State c r s) t = State {cols = c, rows = r, ships = s}
 
 -- IMPLEMENT
 -- Adds hint data to the game state
 hint :: State -> Document -> State
-hint (State l) h = State $ ("Hint " ++ show h) : l
+hint (State c r s) h = State {cols = c, rows = r, ships = s}
 
 -- pridetas
