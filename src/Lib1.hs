aaedc296497cee4ad6f245d9f58929315cf23cdc
Pijus Zlatkus <zlatkus.pijus@gmail.com>
2022-10-01 15:49:37 +0300

    toggle function implementation

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Lib1(
     State, emptyState, gameStart, render, mkCheck, toggle, hint
 ) where
@@ -6,14 +7,16 @@ import Types
 import Data.Char(digitToInt)
 
 data State = State {
-                    cols :: [Int],
-                    rows :: [Int],
-                    ships :: [((Int, Int), Bool)]
-                   }
-                    deriving Show
+    cols :: [Int],
+    rows :: [Int],
+    ships :: [((Int, Int), Bool)]
+} deriving Show
 
 emptyState :: State
-emptyState = State {cols = [], rows = [], ships = []} --pradedam su tusciais masyvais
+emptyState = State {cols = [], rows = [], ships = generateShips} --pradedam su tusciais masyvais, isskyrus ships
+
+generateShips :: [((Int, Int), Bool)]
+generateShips = [((x, y), False) | x <- [0..9], y <- [0..9]]
 
 gameStart :: State -> Document -> State
 gameStart (State c r s) d = gameStart'(State c r s) (show d) --document pakeiciam jo stringu
@@ -51,38 +54,76 @@ writeRows (State c r s) n (x:xs) =
 addRows :: State -> Int -> State
 addRows (State c r s) x = State {cols = c, rows = x : r, ships = s}
 
--- IMPLEMENT
 -- renders your game board
 render :: State -> String
-render (State c r s) = "    0  1  2  3  4  5  6  7  8  9\n    " ++ showCols c ++ showRows r 0 0
+render (State c r s) = "Use toggle command to change ship states: (e. g. \"toggle 24\" -> [x,y])\n  x 0 1 2 3 4 5 6 7 8 9\ny   " ++ showCols c ++ showRows s r 0 ++ "\n"
 
 showCols :: [Int] -> String
 showCols [] = "\n"
-showCols (x:xs) = show x ++ "  " ++ showCols xs
+showCols (x:xs) = show x ++ " " ++ showCols xs
 
-showRows :: [Int] -> Int -> Int -> String -- a ir b kaip x ir y
-showRows [] _ _= " "                           --0  1  2  3  4  5  6  7  8  9
-showRows (x:xs) a b = show a ++ " " ++ show x ++ " __|__|__|__|__|__|__|__|__|__\n" ++ showRows xs (a + 1)
--- kj bus check, implementuoti kad kiekvienai eilutei tikrintu su ships coord ir spausdintu kazka toj vietoj, jei ne tj tsg _|
+showRows :: [((Int, Int), Bool)] -> [Int] -> Int -> String -- a ir b kaip x ir y
+showRows _ [] _ = " "                                      -- 0  1  2  3  4  5  6  7  8  9
+showRows s (x:xs) a = show a ++ " " ++ show x ++ " " ++ printShips s 0 a ++ showRows s xs (a + 1)
 
--- IMPLEMENT
+printShips :: [((Int, Int), Bool)] -> Int -> Int -> [Char]
+printShips s c r =
+    if c <= 9
+        then checkShip s c r : '|' : printShips s (c + 1) r
+        else "\n"
+
+checkShip :: [((Int, Int), Bool)] -> Int -> Int -> Char
+checkShip [] _ _ = '_'
+checkShip (((c, r), b):xs) col row =
+    if col <= 9
+        then
+            if (c == col) && (r == row)
+                then
+                    if (b == True)
+                        then 'X'
+                        else '_'
+                else checkShip xs col row
+        else '_'
+
 -- Make check from current state
 mkCheck :: State -> Check
 mkCheck (State _ _ s) = Check (extractShips s)
---extract coordinates of the ships array where they have been toggled(true)
+
+-- Extract coordinates of the ships array where they have been toggled(true)
 extractShips :: [((Int, Int), Bool)] -> [Coord]
 extractShips [] = []
 extractShips (((col, row), True):xs) = Coord{col = col, row = row} : extractShips xs
 extractShips (((col, row), _):xs) = extractShips xs
--- IMPLEMENT
+
 -- Toggle state's value
--- Receive raw user input tokens
+-- Receive raw user input tokensu
 toggle :: State -> [String] -> State
-toggle (State c r s) t = State {cols = c, rows = r, ships = s}
+toggle (State c r s) [] = State {cols = c, rows = r, ships = s}
+toggle (State c r s) coords = State {cols = c, rows = r, ships = toggleShips s coords}
+
+toggleShips :: [((Int, Int), Bool)] -> [String] -> [((Int, Int), Bool)]
+toggleShips [] _ = []
+toggleShips s [] = s
+toggleShips (x : xs) c = searchKey x c : toggleShips xs c
+
+searchKey :: ((Int, Int), Bool) -> [String] -> ((Int, Int), Bool)
+searchKey s [] = s
+searchKey (c, b) (k : ks) =
+    if (c == (toCoords k)) && (b == False)
+        then (c, True)
+        else
+            if (c == (toCoords k)) && (b == True)
+                then (c, False)
+                else searchKey (c, b) ks
+
+toCoords :: String -> (Int, Int)
+toCoords [] = (-1, -1)
+toCoords (x : xs) =
+    if (length xs) == 1
+        then (digitToInt x, read xs)
+    else (-1, -1)
 
 -- IMPLEMENT
 -- Adds hint data to the game state
 hint :: State -> Document -> State
 hint (State c r s) h = State {cols = c, rows = r, ships = s}
-
--- pridetas
