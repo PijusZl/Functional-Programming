0077a7e2e45c16503fea51974539bac5d58f744f
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-10-17 21:26:15 +0300

    implemented to document, got the older version working

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -2,11 +2,22 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Lib2(renderDocument, hint, gameStart) where
 
-import Types ( ToDocument(..), Document, Check )
-import Lib1 (State(..))
+import Types ( ToDocument(..), Document (..), Check (..), Coord(..) )
+import Lib1 (State(..), gameStart', dListToIntArray, toggleHints)
 
 -- IMPLEMENT
 -- First, make Check an instance of ToDocument class
+
+instance ToDocument Check where
+    toDocument (Check []) = DNull
+    toDocument (Check c) = DList (addToList c)
+
+addToList :: [Coord] -> [Document]
+addToList ((Coord c r):xs) = addCoord c : addCoord r : addToList xs
+addToList [] = []
+
+addCoord :: Int -> Document
+addCoord = DInteger
 
 -- IMPLEMENT
 -- Renders document to yaml
@@ -17,10 +28,11 @@ renderDocument _ = error "Implement me"
 -- This adds game data to initial state
 -- Errors are reported via Either but not error 
 gameStart :: State -> Document -> Either String State
-gameStart (State l) d = Right $ State $ ("Game started: " ++ show d) : l
+gameStart (State c r s) d = Right $ gameStart'(State c r s) (show d)
 
 -- IMPLEMENT
 -- Adds hint data to the game state
 -- Errors are reported via Either but not error 
 hint :: State -> Document -> Either String State
-hint (State l) h = Right $ State $ ("Hint " ++ show h) : l
+hint (State c r s) (DMap ((_ , (DList documents)) : _)) = Right $ State {cols = c, rows = r, ships = toggleHints s (dListToIntArray documents) } -- turi grazint ship'us
+hint s _ = Right s
