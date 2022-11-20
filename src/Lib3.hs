d9c03995b5238c0a19ec2ac688e2bd4909bc28f8
Pijus Zlatkus <zlatkus.pijus@gmail.com>
2022-11-20 20:36:22 +0200

    created instances

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -2,8 +2,8 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Lib3(hint, gameStart, parseDocument, GameStart, Hint) where
 
-import Types ( Document, FromDocument, fromDocument )
-import Lib1 (State(..))
+import Types (Document(..), FromDocument, fromDocument)
+import Lib1 (State(..), gameStart', dListToIntArray, toggleHints)
 
 -- IMPLEMENT
 -- Parses a document from yaml
@@ -13,21 +13,41 @@ parseDocument _ = Left "Implement me"
 -- IMPLEMENT
 -- Change right hand side as you wish
 -- You will have to create an instance of FromDocument
-data GameStart = GameStart deriving Show
+data GameStart = GameStart {
+    start :: Document
+} deriving Show
+
+instance FromDocument GameStart where
+    fromDocument DNull = Left "null Document"
+    fromDocument (DInteger _) = Left "can not read from integer"
+    fromDocument (DString _) = Left "can not read from string"
+    fromDocument (DList _) = Left "can not read from list"
+    fromDocument d = Right (GameStart d)
+
 
 -- This adds game data to initial state
 -- Errors are not reported since GameStart is already totally valid adt
 -- containing all fields needed
 gameStart :: State -> GameStart -> State
-gameStart (State l) d = State $ ("Game started: " ++ show d) : l
+gameStart (State c r s) (GameStart d) = gameStart'(State c r s) (show d)
 
 -- IMPLEMENT
 -- Change right hand side as you wish
 -- You will have to create an instance of FromDocument
-data Hint = Hint deriving Show
+data Hint = Hint {
+    hlist :: [Document]
+} deriving Show
+
+instance FromDocument Hint where
+    fromDocument DNull = Left "null hint Document"
+    fromDocument (DInteger _) = Left "can not get coords from integer"
+    fromDocument (DString _) = Left "can not get coords from string"
+    fromDocument (DList _) = Left "can not get coords from hint list"
+    fromDocument (DMap (("coords" , DList documents) : _)) = Right (Hint documents)
+    fromDocument _ = Left "coords in hint document not found"
 
 -- Adds hint data to the game state
 -- Errors are not reported since GameStart is already totally valid adt
 -- containing all fields needed
 hint :: State -> Hint -> State
-hint (State l) h = State $ ("Hint " ++ show h) : l
+hint (State c r s) (Hint h) = State {cols = c, rows = r, ships = toggleHints s (dListToIntArray h) }
