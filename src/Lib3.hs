957db47301747353f685eb2ce6ecabc4b9e9fe60
Pijus Zlatkus <zlatkus.pijus@gmail.com>
2022-11-23 12:30:00 +0200

    BNF, type parser, yaml test cases

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -5,10 +5,104 @@ module Lib3(hint, gameStart, parseDocument, GameStart, Hint) where
 import Types (Document(..), FromDocument, fromDocument)
 import Lib1 (State(..), gameStart', dListToIntArray, toggleHints)
 
+import Data.Maybe
+import Text.Read
+
 -- IMPLEMENT
 -- Parses a document from yaml
 parseDocument :: String -> Either String Document
-parseDocument _ = Left "Implement me"
+parseDocument [] = Left "empty document"
+parseDocument str = fromParser $ parse str
+    where parse s = do
+            (_, r1) <- parseStartDocument s
+            (d, r2) <- parseDocument' r1
+            return (d, r2)
+
+fromParser :: Either String (Document, String) -> Either String Document
+fromParser (Left s) = Left s
+fromParser (Right (d,_)) = Right d
+
+parseStartDocument :: String -> Either String (Document, String)
+parseStartDocument str = do
+    (_, r1) <- parseChar '-' str
+    (_, r2) <- parseChar '-' r1
+    (_, r3) <- parseChar '-' r2
+    -- optional space
+    (_, r5) <- parseChar '\n' r3
+    return (DNull, r5)
+
+parseDocument' :: String -> Either String (Document, String)
+parseDocument' [] = Right (DNull, "")
+parseDocument' str = checkEOF $ parseDocumentType str
+-- parseDocument' str = orParser (orParser (checkEOF $ parseDocumentType str) (checkEOF $ parseDocumentMap str)) (checkEOF $ parseDocumentList str)
+
+checkEOF :: Either String (Document, String) -> Either String (Document, String)
+checkEOF (Right (d, s))
+            | s == ""   = Right (d, s)
+            | otherwise = Left "expected end of the document"
+checkEOF l = l
+
+parseDocumentType :: String -> Either String (Document, String)
+parseDocumentType str = orParser (orParser (parseDocumentInt str) (parseDocumentNull str)) (parseDocumentString str)
+
+parseDocumentInt :: String -> Either String (Document, String)
+parseDocumentInt str = do
+    (i, r) <- parseInteger str
+    return (DInteger i, r)
+
+parseInteger :: String -> Either String (Int, String)
+parseInteger str =
+    let
+        prefix = takeWhile isNotSeparator str
+    in
+        case prefix of
+            [] -> Left $ "integer expected" -- at line " ++ "" ++ ", character " ++ ""
+            _ -> if isNothing (readMaybe prefix :: Maybe Int)
+                    then Left $ "integer expected"
+                    else Right (read prefix, drop (length prefix) str)
+
+parseDocumentNull :: String -> Either String (Document, String)
+parseDocumentNull str = do
+    (_, r) <- parseNull str
+    return (DNull, r)
+
+parseNull :: String -> Either String (String, String)
+parseNull str = do
+    (_, r1) <- parseChar 'n' str
+    (_, r2) <- parseChar 'u' r1
+    (_, r3) <- parseChar 'l' r2
+    (_, r4) <- parseChar 'l' r3
+    return ("", r4)
+
+parseDocumentString :: String -> Either String (Document, String)
+parseDocumentString str = do
+    (s, r) <- parseString str
+    return (DString s, r)
+
+parseString :: String -> Either String (String, String)
+parseString str =
+    let
+        prefix = takeWhile isNotSeparator str
+    in
+        case prefix of
+            [] -> Left $ "string expected"
+            _  -> Right (prefix, drop (length prefix) str)
+
+isNotSeparator :: Char -> Bool
+isNotSeparator c = c /= ' ' && c /= '\n'
+
+--parseSpace :: String -> Either String (String, String)
+
+parseChar :: Char -> String -> Either String (Char, String)
+parseChar ch [] = Left $ ch : " expected"
+parseChar ch (x:xs) | ch == x   = Right (x, xs)
+                    | otherwise = Left $ ch : " expected"
+
+orParser :: Either String (a, String) -> Either String (a, String) -> Either String (a, String)
+orParser parser1 parser2 =
+    case parser1 of
+        Right a -> Right a
+        Left _ -> parser2
 
 -- IMPLEMENT
 -- Change right hand side as you wish
@@ -50,4 +144,4 @@ instance FromDocument Hint where
 -- Errors are not reported since GameStart is already totally valid adt
 -- containing all fields needed
 hint :: State -> Hint -> State
-hint (State c r s) (Hint h) = State {cols = c, rows = r, ships = toggleHints s (dListToIntArray h) }
+hint (State c r s) (Hint d) = State {cols = c, rows = r, ships = toggleHints s (dListToIntArray d) }
